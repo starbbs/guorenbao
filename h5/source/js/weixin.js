9b171898e9520fa041b76b73e09480cf21b1b812
ccforeverd <zh1045456074@163.com>
2016-01-13 18:57:30 +0800

    2016年01月13日 修改
    1. 添加消息页面
    2. 添加button.error样式
    3. 去掉微信调试模式

--- h5/source/js/weixin.js
+++ h5/source/js/weixin.js
@@ -68,7 +68,7 @@ define('h5-weixin', ['api'], function(api) {
 		if (data.status == 200) {
 			var returnData = data.data.signatureData;
 			wx.config({
-				debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+				debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
 				appId: weixin.appId = returnData.appId,				// 必填，公众号的唯一标识
 				timestamp: weixin.timestamp = returnData.timestamp,	// 必填，生成签名的时间戳
 				nonceStr: weixin.nonceStr = returnData.nonceStr,	// 必填，生成签名的随机串
