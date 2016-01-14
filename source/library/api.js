4fd5370494d423f23dd8b4198aba71f3b3c0116e
余效俭 <ijieshu@163.com>
2016-01-14 10:56:58 +0800

    api修改
    api修改

--- source/library/api.js
+++ source/library/api.js
@@ -8,8 +8,7 @@ define('api', ['cookie', 'h5-wait'], function() {
 	// var basePath = '.'; // 同域
 	// var basePath = 'http://172.16.27.110:8080/'; // 郑明海本地
 	// var basePath = typeof __basePath === 'undefined' ? 'http://www.goopal.me' : __basePath;
-//	var basePath = 'http://www.goopal.me';
-	var basePath = 'http://172.16.14.28:';
+	var basePath = 'http://www.goopal.me';
 	/** [api 接口集合] */
 	var api = {};
 
