1ac4e0b1e8473e0dd72eba25d7d22c508c958233
xltsky <xltsky@163.com>
2016-03-17 09:28:54 +0800

    2016年3月17日
    修改交易所的代码

--- exchange/factory/pages/index/index.js
+++ exchange/factory/pages/index/index.js
@@ -1,8 +1,6 @@
 require(['api_mkt','mkt_info','cookie'], function(api_mkt,mkt_info) {
-	//alert('index');
-	//api_mkt
-	console.log(api_mkt);
-	console.log(mkt_info);
-
-	console.log(mkt_info.get());
+	//console.log(api_mkt);
+	//console.log(mkt_info);
+	mkt_info.get();
+	
 });
--- exchange/source/js/info.js
+++ exchange/source/js/info.js
@@ -1,36 +1,45 @@
 define('mkt_info', ['api_mkt'], function(api_mkt) {
 	var price = {
-		interval: 3000, // 请求间隔
+		interval: 1000, // 请求间隔
 		timer: null, // 定时器
-		now: false, // 当前价格
 		stop: function() {
 			clearTimeout(price.timer);
 		},
-		//onUpdate: $.noop, // 价格更新时回调, 传参(当前价格, 上一次价格)
 		onChange: updateprice, // 价格改变时回调, 传参(当前价格, 上一次价格, 改变大小)
-		//onFirstChange: $.noop, // 第一次改变时回调, 传参(当前价格)
 	};
 	var once = price.once = function(callback) {
 		api_mkt.pollinfo(function(data) {
-			if (data.status == 200) {
-				callback && callback(data.data.price);
-			} else {
-				console.log(data);
-			}
+			//if (data.status == 200) {
+			//console.log(data);
+			callback && callback(data);
+			// if(1){
+			// 	callback && callback("asfdjkl");
+			// } else {
+			// 	console.log(data);
+			// }
 		});
 	};
-
-	var updateprice = function(callback){
-		console.log('asdfjkl');
-
+	var updateprice = function(haha){
+		var thelatestprice = JSON.parse(haha['order'][0]).price;
+		$('#thelatestprice').html(thelatestprice);      //最新成交价赋值
+		var turnover = Number(haha['24Total']).toFixed(2);
+		$('#turnover').html(turnover);  		        //24小时成交量
+		var low24 = Number(haha['24low']);
+		var high24 = Number(haha['24high']);
+		$('#thehighest_price').html(low24.toFixed(2));  //
+		$('#thelowest_price').html(high24.toFixed(2));  //
+		var high24 = Number(haha['24high']);
+		
+		var total = Number(haha['total']);
+		var unknow = Number((thelatestprice/24)-1);
+		$('#cumulativevolume').html(total.toFixed(2));
+		$('#pricechangeratio').html(unknow.toFixed(2)+"%");
 	}
-
 	var get = price.get = function() {
 		once(function(next){
-			console.log(next);
+			updateprice(next);
+			price.timer = setTimeout(price.get, price.interval);
 		});
-
-
 		// once(function(next) {
 		// 	var now = price.now;
 		// 	var change = next - now;
