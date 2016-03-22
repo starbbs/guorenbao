9528a310c3c422fda69cf4242bf3ae9c57bfe737
CWSchen <1044502604@qq.com>
2016-03-22 20:25:32 +0800

    2016年3月22日

--- exchange/factory/pages/regist/regist.js
+++ exchange/factory/pages/regist/regist.js
@@ -13,9 +13,14 @@ require(['api_mkt','cookie'], function(api_mkt) {
 		}	
 	});
 	//果仁协议
-	$(".markasread").click(function(){
-		$(".bottom_tip").toggle();
+	$(".markasread").click(function(e){
+		$(".bottom_tip").toggle();       
+        e.stopPropagation(); 
 	});
+	//点击其他位置，收起
+    $('body').bind('click', function(event) {  
+        $('.bottom_tip').hide();
+    });
 		
 	//给所有regist_rg_input类+blur
 	$(".regist_rg_input").blur(function(){
