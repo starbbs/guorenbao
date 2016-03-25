ad1198481603b23469ffd7e8431f469007010bde
CWSchen <1044502604@qq.com>
2016-03-25 14:33:13 +0800

    2016年3月25日

--- exchange/factory/pages/mine/basicinfo/basicinfo.js
+++ exchange/factory/pages/mine/basicinfo/basicinfo.js
@@ -1,3 +1,45 @@
 require(['api_mkt','cookie'], function(api_mkt) {
+
+    $(".bg").width($(document).width());
+    $('.bg').height($(document).height());
+
+    console.log($(document).width());
+    console.log($(document).height());
+
+    $('.bg').css('left', 0);
+    $('.bg').css('top', 0);
+
+    $(".close_btn").on("click",function(){
+        $("#popDiv").hide();
+        $("#bg").hide();
+    });
+
+    var flag = true;
+    $('.messagenum_area').on("click",function(){
+        if(flag){
+            flag = false;
+            $(this).css("background-color","#ffffff");
+            $(".popup_message_box").show("100");
+            $(".messagenum_area em").css("color","#333333");
+            $(".msg_num").css("color","#333333");
+        } else {
+            flag = true;
+            $(this).css("background-color","#282828");
+            $(".popup_message_box").hide("100");
+            $(".messagenum_area em").css("color","#cccccc");
+            $(".msg_num").css("color","#cccccc");
+        }
+    });
+    function showDiv() {
+        $("#bg").show();
+        $("#popDiv").fadeIn(500);
+    }
+
+    function closeDiv() {
+        $("#popDiv").hide();
+        $("#bg").hide();
+    }
+
+	
 	
 });
--- exchange/factory/pages/mine/certification/certification.js
+++ exchange/factory/pages/mine/certification/certification.js
@@ -1,3 +1,47 @@
 require(['api_mkt','cookie'], function(api_mkt) {
 	
+	//我的账户-实名验证校验
+	var BtnConfirm = false;
+	//校验姓名
+	$('#realAuthName').blur(function(){
+		var realAuthName = $(this).val();
+		var reg = /^[\u4e00-\u9fa5]+$/;   
+        if(!reg.test(realAuthName)){
+			BtnConfirm = false;
+			$('.msg-realAuthName').text('请输入姓名');
+		}else{
+			BtnConfirm = true;
+			$('.msg-realAuthName').text('');
+		}
+	});
+	//校验身份证号
+	$('#realAuthId').blur(function(){
+		var realAuthId = $(this).val();
+		var reg = /^(\d{15}$|^\d{18}$|^\d{17}(\d|X|x))$/;   
+        if(!reg.test(realAuthId)){
+			BtnConfirm = false;
+			$('.msg-realAuthId').text('请输入正确的身份证号');
+		}else{
+			BtnConfirm = true;
+			$('.msg-realAuthId').text('');
+		}
+	});
+
+	//按钮 
+	$('.realAuthBtn').click(function(){
+		if(BtnConfirm == false){
+			alert('请填写正确姓名和身份证号');
+		}else{
+			//我的账户实名认证信息
+			api_mkt.realAuth(function(data) {
+				console.log(data);
+		        if(data.status == 200){
+		        	alert('success');               
+		        }else{
+		        	alert('err');
+		        }
+			});
+		}
+	});
+
 });
