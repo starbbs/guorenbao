961d0311174db229a4261382ae2773f4dd169fec
ccforeverd <zh1045456074@163.com>
2016-03-29 11:29:29 +0800

    版本号回退问题

--- h5/factory/views/agreement/agreement.js
+++ h5/factory/views/agreement/agreement.js
@@ -3,7 +3,8 @@
 // H5微信端 --- view-agreement 关于我们
 
 
-define('h5-view-agreement', ['h5-view'], function(View) {
+define('h5-view-agreement', ['h5-view', 'get'], function(View, get) {
 	var agreement = new View('agreement');
+	agreement.v = $('#js-v').html('V' + (get.data.v || '1.0'));
 	return agreement;
 });
