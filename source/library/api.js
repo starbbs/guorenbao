61cf91d0b7e373e94ee46c3f8b6b02034ebfbc6e
ccforeverd <zh1045456074@163.com>
2016-03-29 11:11:41 +0800

    添加https测试地址

--- source/library/api.js
+++ source/library/api.js
@@ -7,7 +7,8 @@ define('api', ['cookie', 'filters', 'h5-alert', 'h5-wait'], function() {
 
 	// var basePath = '.'; // 同域
 	// var basePath = 'http://116.213.142.89:8080'; // http测试服务器
-	var basePath = 'https://endpoint.goopal.com.cn/'; // https测试服务器
+	var basePath = 'https://endpoint.goopal.com.cn/'; // https正式服务器
+	// var basePath = 'https://www.yuxiaojian.cn/'; // https测试服务器
 
 	/** [api 接口集合] */
 	var api = {};
