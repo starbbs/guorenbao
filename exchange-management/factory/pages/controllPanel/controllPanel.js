cf85b01ba7899d9093c6e26a0886f6d42c08f702
CWSchen <1044502604@qq.com>
2016-03-31 11:22:59 +0800

    2016年3月31日

--- exchange-management/factory/pages/controllPanel/controllPanel.js
+++ exchange-management/factory/pages/controllPanel/controllPanel.js
@@ -1,17 +1,71 @@
-require(['jquery','avalon'],function($,avalon){
+require(['api_mkt_management'], function(api_mkt_management) {
 
-    //控制面板右侧 常用 的显示隐藏
-        $(".aside-div-spanRight").hover(function(){
-            $(".aside-div-spanRight-div").show();
-        },function(){
-            var timer = setTimeout(function(){
-                $(".aside-div-spanRight-div").hide();
-            },500);
-            $(".aside-div-spanRight-div").hover(function(){
-            $(".aside-div-spanRight-div").show();
-            clearInterval(timer);
-            });
+    //后台重置密码
+    $('#btnResetPwd').click(function(){
+        api_mkt_management.setLoginPassword({
+            'oldPassword':$('#oldPwd').val(),
+            'newPassword':$('#newPwd').val()
+        },function(data) {
+            if (data.status == 200) {
+                console.log(data);
+                //$.cookie('key','');
+                window.location.href="controllPanel.html";
+            } else {
+                alert('输入原密码有误！');
+                console.log(data.msg);
+            }
         });
+    });
+
+    //后台创建管理员
+    $('#btnCreateManager').click(function(){
+        api_mkt_management.create({
+            'userName':$('#CreateManagerUserName').val(),
+            'password':$('#CreateManagerPwd').val(),
+            'role':$('.selectRole').find('option:selected').text(),
+            'phone':$('#CreateManagerPhone').val()
+        },function(data) {
+            if (data.status == 200) {
+                console.log(data);
+                //$.cookie('key','');
+                window.location.href="controllPanel.html";
+            } else {
+                //alert('输入原密码有误！');
+                console.log(data.msg);
+            }
+        });
+    });
+    //后台锁定管理员
+    $('#btnLock').click(function(){
+        api_mkt_management.lockAdmin({
+            'adminId':$('.btnLockManager').find('option:selected').text()
+        },function(data) {
+            if (data.status == 200) {
+                console.log(data);
+                //$.cookie('key','');
+                window.location.href="controllPanel.html";
+            } else {
+                //alert('输入原密码有误！');
+                console.log(data.msg);
+            }
+        });
+    });
+    //后台解锁管理员
+    $('#btnUnlock').click(function(){
+        api_mkt_management.unlockAdmin({
+            'adminId':$('.btnUnlockManager').find('option:selected').text()
+        },function(data) {
+            if (data.status == 200) {
+                console.log(data);
+                //$.cookie('key','');
+                window.location.href="controllPanel.html";
+            } else {
+                //alert('输入原密码有误！');
+                console.log(data.msg);
+            }
+        });
+    });
+    
  
   
 //end
