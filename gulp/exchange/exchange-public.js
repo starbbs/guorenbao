93c67d7a22ea73ff92caf1e8c17e5c0cf01f955d
xltsky <xltsky@163.com>
2016-04-05 13:02:55 +0800

    2016年4月5日
    更新

--- gulp/exchange/exchange-public.js
+++ gulp/exchange/exchange-public.js
@@ -5,29 +5,78 @@ var path = require('path');
 var tools = require('../tools');
 var paths = require('./exchange-paths');
 var uglify = require('gulp-uglify');
+var rjs = require('gulp-requirejs');
+var imagemin = require('gulp-imagemin');
 gulp.task('exchange-js', function() {
 	return gulp.src(path.join(paths.build, '/js/*.js'))
 		// .pipe(uglify())
 		.pipe(gulp.dest(paths.public + '/js'))
 });
 gulp.task('exchange-rjs', ['exchange-js'], function() {
-	return tools.rjs(paths.build + '/*.html', paths.public + '/js', {
-		rjsPaths: paths.rjs,
-		uglify: true
+	return gulp.src([
+
+		paths.build + '/js/basicinfo.js',
+		paths.build + '/js/certification.js',
+		paths.build + '/js/cnydepositswithdrawal.js',
+		paths.build + '/js/cnydepositswithdrawal_new.js',
+		paths.build + '/js/common.js',
+		paths.build + '/js/conditionofassets.js',
+		paths.build + '/js/conditionofassets_new.js',
+		paths.build + '/js/footer.js',
+		paths.build + '/js/index.js',
+		
+		paths.build + '/js/judge_certification.js',
+		paths.build + '/js/judge_login.js',
+		paths.build + '/js/modifyloginpassword.js',
+		paths.build + '/js/modifypaymentcode.js',
+		paths.build + '/js/nutstopupwithdrawal.js',
+		paths.build + '/js/nutstopupwithdrawal_new.js',
+		paths.build + '/js/pagehead.js',
+		paths.build + '/js/regist.js',
+		paths.build + '/js/resetloginpwd.js',
+		paths.build + '/js/resetpaymentcode.js',
+		paths.build + '/js/ssmessage.js',
+		paths.build + '/js/tradingfloor.js',
+		paths.build + '/js/tradingfloor_new.js',
+		paths.build + '/js/withdraw.js',
+		paths.build + '/js/withdraw_new.js',
+		paths.build + '/js/withdraw-city.js'
+	], function(somethingNULL, filePaths) {
+		filePaths.forEach(function(url) {
+			var name = path.basename(url, '.js');
+			rjs({
+				baseUrl: paths.build + '/js',
+				// mainConfigFile: '../config.js',
+				name: name,
+				out: name + '.js',
+				paths: paths.rjs,
+				shim: {
+					jquery: {
+						exports: "jQuery"
+					},
+					$: {
+						exports: "jQuery"
+					}
+				},
+				include: [],
+				module: [],
+				// optimize: 'uglify'
+			})
+			.pipe(uglify())
+			.pipe(gulp.dest(paths.public + '/js'));
+		});
 	});
 });
 
 gulp.task('exchange-html', function() {
 	return tools.html(paths.build + '/*.html', paths.public, {
-		remove: '<script src="./js/config.js"></script>',
+		remove: '<script type="text/javascript" src="./js/config.js"></script>',
 	});
 });
 gulp.task('exchange-img', function() {
-	return tools.fileMove(paths.build + '/images/**/**', paths.public + '/images', {
-		type: 'image',
-		imagemin: true,
-		removeDirname: false
-	});
+	return gulp.src(paths.build + '/images/**/**')
+		.pipe(imagemin())
+		.pipe(gulp.dest(paths.public + '/images'))
 });
 
 gulp.task('exchange-css', function() {
