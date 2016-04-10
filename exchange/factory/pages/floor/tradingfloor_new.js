07df39c2fc6ff19a3bf862610014424b0202106d
CWSchen <1044502604@qq.com>
2016-04-10 16:36:22 +0800

    2016年4月10日 cws

--- exchange/factory/pages/floor/tradingfloor_new.js
+++ exchange/factory/pages/floor/tradingfloor_new.js
@@ -937,6 +937,45 @@ require(['api_mkt', 'mkt_info', 'mkt_trade', 'cookie'], function(api_mkt, mkt_in
     $('.sel_div_password').focus(function(){
         $(this).val('');
     });
+        //接受跳转参数
+    $(function() {
+        function getQueryString(name) {
+            href = decodeURIComponent(location.href);
+            // 如果链接没有参数，或者链接中不存在我们要获取的参数，直接返回空
+            if (href.indexOf("?") == -1 || href.indexOf(name + '=') == -1) {
+                return '';
+            }
+            // 获取链接中参数部分
+            var queryString = href.substring(href.indexOf("?") + 1);
+            // 分离参数对 ?key=value&key2=value2
+            var parameters = queryString.split("&");
+            var pos, paraName, paraValue;
+            for (var i = 0; i < parameters.length; i++) {
+                // 获取等号位置
+                pos = parameters[i].indexOf('=');
+                if (pos == -1) {
+                    continue;
+                }
+                // 获取name 和 value
+                paraName = parameters[i].substring(0, pos);
+                paraValue = parameters[i].substring(pos + 1);
+                // 如果查询的name等于当前name，就返回当前值，同时，将链接中的+号还原成空格
+                if (paraName == name) {
+                    return unescape(paraValue.replace(/\+/g, " "));
+                }
+            }
+            return '';
+        };
+
+        var b = getQueryString("whichtab");
+        console.log(b);
+        if (b) {
+            $('.tab-li:eq(1)').addClass('bottomon');
+            $('.tab-li:eq(0)').removeClass('bottomon');
+            $('.entrust-side-table:eq(0)').hide();
+            $('.entrust-side-table:eq(1)').show();
+        }
+    });
 
 
 });
